f9904ba72c4dc55a180f7386d9f46c13a008022b
davidladeji <71533464+davidladeji@users.noreply.github.com>
2025-06-05 21:19:08 -0700

    fix: Cleanup floating Icons
    Note: Consider downloading these icons in case links don't stay online forever

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -58,24 +58,41 @@ const Hero = () => {
               <div className="absolute inset-0 bg-gradient-to-br from-blue-400 via-purple-500 to-pink-500 rounded-full opacity-20 animate-pulse"></div>
               <div className="absolute inset-4 bg-gradient-to-tr from-cyan-400 to-blue-500 rounded-full opacity-30 animate-bounce"></div>
               <div className="absolute inset-8 bg-white rounded-full shadow-2xl flex items-center justify-center">
-                <div className="text-6xl">👨‍💻</div>
+                {/* <div className="text-6xl">👨‍💻</div> */}
+                <img
+                  src="https://static.vecteezy.com/system/resources/previews/004/491/062/non_2x/programmer-working-concept-for-web-banner-man-work-with-code-and-programming-at-computer-creates-software-modern-person-scene-illustration-in-flat-cartoon-design-with-people-characters-vector.jpg"
+                  alt="3D Character"
+                  className="w-72 h-72 md:w-96 md:h-32 object-contain"
+                />
               </div>
               
               {/* Floating Tech Icons - Only 2 bouncing for less distraction */}
-              <div className="absolute -top-4 -right-4 w-12 h-12 bg-gray-800 rounded-lg flex items-center justify-center shadow-lg animate-bounce">
-                <span className="text-yellow-400 font-bold">JS</span>
+              <div className="absolute -top-4 -right-4 w-12 h-12 bg-gradient-to-r from-yellow-400 to-orange-500 rounded-lg flex items-center justify-center shadow-lg animate-bounce">
+                <span className="text-white font-bold">JS</span>
               </div>
+              {/* JAVA LOGO */}
               <div className="absolute -bottom-4 -left-4 w-12 h-12 bg-blue-600 rounded-lg flex items-center justify-center shadow-lg animate-bounce delay-300">
-                <span className="text-cyan-300 font-bold">⚛️</span>
+                <span className="text-cyan-100 font-bold">
+                  <img width="36" height="36" src="https://img.icons8.com/dusk/64/java-coffee-cup-logo.png" alt="java-coffee-cup-logo"/>
+                </span>
               </div>
-              <div className="absolute top-1/2 -left-8 w-10 h-10 bg-amber-600 rounded-lg flex items-center justify-center shadow-lg">
-                <span className="text-red-800 font-bold text-xs">☕</span>
+              {/* REACT LOGO */}
+              <div className="absolute top-1/2 -left-8 w-10 h-10 bg-pink-200 rounded-lg flex items-center justify-center shadow-lg">
+                <span className="text-red-800 font-bold">
+                  <img width="36" height="36" src="https://img.icons8.com/external-tal-revivo-color-tal-revivo/24/external-react-a-javascript-library-for-building-user-interfaces-logo-color-tal-revivo.png" alt="external-react-a-javascript-library-for-building-user-interfaces-logo-color-tal-revivo"/>
+                </span>
               </div>
-              <div className="absolute top-1/4 -right-8 w-10 h-10 bg-gray-800 rounded-lg flex items-center justify-center shadow-lg">
-                <span className="text-blue-400 font-bold text-xs">🐬</span>
+              {/* MYSQL LOGO */}
+              <div className="absolute top-1/4 -right-8 w-10 h-10 bg-white-600 rounded-lg flex items-center justify-center shadow-lg">
+                <span className="text-blue-400 font-bold">
+                  <img width="36" height="36" src="https://img.icons8.com/external-those-icons-lineal-color-those-icons/24/external-MySQL-programming-and-development-those-icons-lineal-color-those-icons.png" alt="external-MySQL-programming-and-development-those-icons-lineal-color-those-icons"/>
+                </span>
               </div>
-              <div className="absolute bottom-1/4 -right-8 w-10 h-10 bg-gray-800 rounded-lg flex items-center justify-center shadow-lg">
-                <span className="text-yellow-400 font-bold text-xs">🐍</span>
+              {/* FIX PYTHON LOGO */}
+              <div className="absolute bottom-1/4 -right-8 w-10 h-10 bg-green-300 rounded-lg flex items-center justify-center shadow-lg">
+                <span className="text-yellow-400 font-bold text-xs">
+                  <img src="https://img.icons8.com/?size=100&id=13441&format=png&color=000000"/>
+                </span>
               </div>
             </div>
           </div>
